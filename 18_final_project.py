546c18ae3e8d7fc552706510544fcd0f38e83c20
erictodd <39839921+erictodd@users.noreply.github.com>
2019-05-06 12:47:13 -0500

    Add files via upload

--- 18_final_project.py
+++ 18_final_project.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2016-2017 Anki, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License in the file LICENSE.txt or at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-'''Use custom objects to create a wall in front of Cozmo.
-
-This example demonstrates how you can create custom objects in the world, and
-automatically have Cozmo path around them as if they are real obstacles.
-
-It creates a wall in front of cozmo and tells him to drive around it.
-He will plan a path to drive 200mm in front of himself after these objects are created.
-
-The `use_3d_viewer=True` argument causes the 3D visualizer to open in a new
-window - this shows where Cozmo believes this imaginary object is.
-'''
 import time
 import asyncio
 import cozmo
@@ -89,27 +65,17 @@ def cozmo_program(robot: cozmo.robot.Robot):
     while not cube: 
         try:
             cube = robot.world.wait_for_observed_light_cube(timeout=30)
-            print("Found cube", cube)
+            print("Found it", cube)
 
         except asyncio.TimeoutError:
-		#robot.go_to_pose(Pose(500, 0, 0, angle_z=degrees(0)), relative_to_robot=False).wait_for_completed()
-            print("Didn't find a cube :-(")
+            print("Didn't find it")
             look_around.stop()
             robot.go_to_pose(Pose(xpose, 0, 0, angle_z=degrees(0)), relative_to_robot=False).wait_for_completed()
             look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
             xpose = 1000
-            #pass
-
-        #finally:
-		# whether we find it or not, we want to stop the behavior
 
 
-    #if cube is None:
-        #robot.go_to_pose(Pose(500, 0, 0, angle_z=degrees(0)), relative_to_robot=False).wait_for_completed()
-     #   robot.play_anim_trigger(cozmo.anim.Triggers.MajorFail)
-     #   return
-
-    print("Yay, found cube")
+    print("Yay")
     look_around.stop()
 
     cube.set_lights(cozmo.lights.green_light.flash())
